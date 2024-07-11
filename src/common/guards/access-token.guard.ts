b267280893397bac7c8211e3a50d6c2aeb0d6ad8
Sokratis <sokratis.metallidis@agilox.net>
2024-07-11 16:59:35 +0200

    Add access token guard

---
+++ src/common/guards/access-token.guard.ts
@@ -0,0 +1,45 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import jwtConfig from '../../iam/config/jwt.config';
+import { REQUEST_USER_KEY } from '../../iam/iam.constants';
+import { Request } from 'express';
+
+@Injectable()
+export class AccessTokenGuard implements CanActivate {
+  constructor(
+    private jwtService: JwtService,
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest<Request>();
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+
+    try {
+      request[REQUEST_USER_KEY] = await this.jwtService.verifyAsync(
+        token,
+        this.jwtConfiguration,
+      );
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [_, token] = request.headers.authorization?.split(' ') ?? [];
+    return token;
+  }
+}
