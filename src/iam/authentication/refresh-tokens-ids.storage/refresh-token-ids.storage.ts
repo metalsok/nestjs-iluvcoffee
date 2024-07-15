b538d89ed8ce249fdc059e7978c1269143bd54ed
Sokratis <sokratis.metallidis@agilox.net>
2024-07-15 17:28:55 +0200

    add redis api service

---
+++ src/iam/authentication/refresh-tokens-ids.storage/refresh-token-ids.storage.ts
@@ -0,0 +1,36 @@
+import { OnApplicationBootstrap, OnApplicationShutdown } from '@nestjs/common';
+import Redis from 'ioredis';
+
+export class RefreshTokenIdsStorage
+  implements OnApplicationBootstrap, OnApplicationShutdown
+{
+  private redisClient: Redis;
+
+  onApplicationShutdown(signal?: string) {
+    this.redisClient.quit();
+  }
+
+  onApplicationBootstrap() {
+    this.redisClient = new Redis({
+      host: 'localhost', // NOTE: According to best practices, we should use the environment variables here instead.
+      port: 6379, // 👆
+    });
+  }
+
+  async insert(userId: number, tokenId: string): Promise<void> {
+    await this.redisClient.set(this.getKey(userId), tokenId);
+  }
+
+  async validate(userId: number, tokenId: string): Promise<boolean> {
+    const storeId = await this.redisClient.get(this.getKey(userId));
+    return storeId === tokenId;
+  }
+
+  async invalidate(userId: number): Promise<void> {
+    await this.redisClient.del(this.getKey(userId));
+  }
+
+  private getKey(userId: number): string {
+    return `user-${userId}`;
+  }
+}
